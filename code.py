eb0928f74b0c0cb387cffc6632becfb62c467ea7
Dorota Jarecka <djarecka@gmail.com>
2017-11-09 22:38:41 -0500

    celaning

--- code.py
+++ code.py
@@ -1,8 +1,5 @@
-def my_factorial(n):
-    result = 1
-    for i in range(1, n+1):
-        result = result * i
-    return result
+
+
 def my_factorial(n):
     if n < 0:
         raise Exception("factorial can be calculated only for positive numbers")
