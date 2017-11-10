64529e7d41e0def03f3e84e4a5c500202706c8aa
Dorota Jarecka <djarecka@gmail.com>
2017-11-09 22:34:51 -0500

    testing function

---
+++ test_factorial.py
@@ -0,0 +1,14 @@
+from code import my_factorial
+
+def test_factorial():
+    assert my_factorial(1) == 1
+    assert my_factorial(5) == 120
+
+def test_factorial_large():
+    assert my_factorial(10000) > 0
+
+import pytest
+
+def test_factorial_negative():
+    with pytest.raises(Exception):
+       my_factorial(-10)
